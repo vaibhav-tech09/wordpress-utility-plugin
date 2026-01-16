ea4aab9aeec8822a10b0a06b43fe18b87c1110db
vaibhav-tech09 <09singh2019vs@gmail.com>
2026-01-16 22:41:01 +0530

    Initial commit: WordPress utility plugin

---
+++ README.md
@@ -0,0 +1,14 @@
+# WordPress Utility Plugin
+
+A custom WordPress plugin developed using PHP to extend WordPress functionality.
+
+## Features
+- Adds a reusable custom shortcode
+- Clean and minimal plugin structure
+- Easy to extend for future features
+
+## Usage
+Use shortcode: [utility]
+
+## Tech Stack
+PHP, WordPress
---
+++ wp-utility-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: WP Utility Plugin
+Description: A simple WordPress utility plugin that adds a custom shortcode.
+Version: 1.0
+Author: Vaibhav Singh
+*/
+
+function wp_utility_shortcode() {
+    return "<p>This content is added using a custom WordPress plugin shortcode.</p>";
+}
+
+add_shortcode('utility', 'wp_utility_shortcode');
